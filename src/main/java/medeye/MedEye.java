67e0b35ebb3da9484459ddcf4ac9b2f9f0285932
PhaseRush <leozhang888@yahoo.ca>
2019-02-09 18:18:35 -0800

    Text Recognition works :)

--- src/main/java/medeye/MedEye.java
+++ src/main/java/medeye/MedEye.java
@@ -1,15 +1,52 @@
 package medeye;
 
-import java.awt.image.BufferedImage;
+import com.google.cloud.vision.v1.*;
+import com.google.protobuf.ByteString;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MedEye {
 
-    public static void main(String[] args) {
-        // load file
+    public static void main(String[] args) throws IOException {
+        // https://cloud.google.com/docs/authentication/production#auth-cloud-implicit-java
+        // Instantiates a client
+        try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
 
-        String path = "MedEye_Images/test1.png";
-        BufferedImage helloImg = Utility.loadImageFromPath(path);
+            // The path to the image file to annotate
+            String fileName = "MedEye_Images/test2small.png";
 
-        System.out.println("Height x Width: " + helloImg.getHeight() + " x " + helloImg.getWidth());
+            // Reads the image file into memory
+            Path path = Paths.get(fileName);
+            byte[] data = Files.readAllBytes(path);
+            ByteString imgBytes = ByteString.copyFrom(data);
+
+            // Builds the image annotation request
+            List<AnnotateImageRequest> requests = new ArrayList<>();
+            Image img = Image.newBuilder().setContent(imgBytes).build();
+            Feature feat = Feature.newBuilder().setType(Feature.Type.TEXT_DETECTION).build();
+            AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
+                    .addFeatures(feat)
+                    .setImage(img)
+                    .build();
+            requests.add(request);
+
+            // Performs label detection on the image file
+            BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    System.out.printf("Error: %s\n", res.getError().getMessage());
+                    return;
+                } else {
+                    System.out.println(res.getFullTextAnnotation().getText());
+                }
+            }
+        }
     }
 }
