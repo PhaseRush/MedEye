224a10a017e20f6f8da3782900fc2f2fd3fde445
kkBOSS <nicole99uty@gmail.com>
2019-02-09 16:23:29 -0800

    test images

--- src/main/java/medeye/MedEye.java
+++ src/main/java/medeye/MedEye.java
@@ -7,7 +7,7 @@ public class MedEye {
     public static void main(String[] args) {
         // load file
 
-        String path = "MedEye_Images/hello.png";
+        String path = "MedEye_Images/test1.png";
         BufferedImage helloImg = Utility.loadImageFromPath(path);
 
         System.out.println("Height x Width: " + helloImg.getHeight() + " x " + helloImg.getWidth());
